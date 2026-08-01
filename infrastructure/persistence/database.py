17ce4921d64e1bd31c2da405e02a53279bef10d1
lasto4kaqwert <lasto4kaqwert@gmail.com>
2026-08-01 21:26:01 +0300

    database_url with asyncpg for database

--- infrastructure/persistence/database.py
+++ infrastructure/persistence/database.py
@@ -1,3 +1,4 @@
+from sqlalchemy.engine import make_url
 from sqlalchemy.ext.asyncio import (
     AsyncSession,
     async_sessionmaker,
@@ -8,9 +9,11 @@ from settings import Settings
 
 settings = Settings()
 
-engine = create_async_engine(
-    str(settings.database_url),
+database_url = make_url(settings.database_url).set(
+    drivername="postgresql+asyncpg",
 )
+
+engine = create_async_engine(str(database_url))
 
 
 async_session_factory = async_sessionmaker(
